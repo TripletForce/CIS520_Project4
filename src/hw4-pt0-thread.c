4fb2419cd4bf8d01e681fc6cb07e85663ed7c3bb
Andrew Barrett <ambarret@ksu.edu>
2024-04-07 18:41:41 -0500

    added pthreads to hw4-pt0-thread.c

--- src/hw4-pt0-thread.c
+++ src/hw4-pt0-thread.c
@@ -40,6 +40,40 @@ void init_arrays()
   }
 }
 
+void *count_array(void *arg) {
+    int myID = *((int *) arg); // Casting the argument to int
+    char theChar;
+    int i, j, charLoc;
+    int local_char_count[ALPHABET_SIZE];
+
+    int startPos = myID * (ARRAY_SIZE / NUM_THREADS);
+    int endPos = startPos + (ARRAY_SIZE / NUM_THREADS);
+
+    printf("Thread %d: startPos = %d, endPos = %d \n", myID, startPos, endPos);
+
+    // Init local count array
+    for (i = 0; i < ALPHABET_SIZE; i++) {
+        local_char_count[i] = 0;
+    }
+
+    // Count up our section of the global array
+    for (i = startPos; i < endPos; i++) {
+        for (j = 0; j < STRING_SIZE; j++) {
+            theChar = char_array[i][j];
+            charLoc = ((int) theChar) - 97;
+            local_char_count[charLoc]++;
+        }
+    }
+
+    // Sum up the partial counts into the global arrays
+    for (i = 0; i < ALPHABET_SIZE; i++) {
+        char_counts[i] += local_char_count[i];
+    }
+
+    pthread_exit(NULL); // Exiting thread
+}
+
+/* Old count_array method
 void count_array(int myID)
 {
   char theChar;
@@ -68,6 +102,7 @@ void count_array(int myID)
      char_counts[i] += local_char_count[i];
   }
 }
+*/
 
 void print_results()
 {
@@ -82,13 +117,32 @@ void print_results()
 }
 
 int main() {
+	pthread_t threads[NUM_THREADS]; // Array to hold thread IDs
+    int thread_args[NUM_THREADS]; // Array to hold thread arguments
 	int i;
 
+	init_arrays();
+
+	// Create threads
+    for (i = 0; i < NUM_THREADS; i++) {
+        thread_args[i] = i;
+        pthread_create(&threads[i], NULL, count_array, (void *) &thread_args[i]);
+    }
+
+    // Join threads
+    for (i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+	/* main before the pthreads
 	init_arrays();
 	for (i = 0; i < NUM_THREADS; i++ ) {
 		count_array(i);
 	}
-    
+    */
+
 	print_results();
+	
+	pthread_exit(NULL);
 }
 
